b6afb8f8d28354d0246f4b18fc7d6f6945838245
Pavinee <Pavinee@137.43.159.39>
2018-02-06 10:21:00 +0000

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name="systeminfo1",
+      version="0.1",
+      description="Basic system information for COMP30670",
+      url="",
+      author="Pavinee Kavanagh",
+      author_email="pavinee.kavanagh@ucdconnect.ie",
+      licence="GPL3",
+      packages=['systeminfo'],
+      entry_points={
+          'console_scripts':['comp30670_systeminfo1=systeminfo.main:main'],
+        }
+      )
---
+++ systeminfo/main.py
@@ -0,0 +1,14 @@
+'''
+Created on 6 Feb 2018
+
+@author: Pavinee
+'''
+import platform
+
+def main():
+    print(platform.platform())
+    return 
+
+
+if __name__ == '__main__':
+    main()
